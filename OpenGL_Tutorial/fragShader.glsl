7627cbab4a5b9ed3eebec3998b8c15e26dea72d3
Mumei <mumeii452@gmail.com>
2023-12-09 10:16:22 +0900

    2023/12/09

--- OpenGL_Tutorial/fragShader.glsl
+++ OpenGL_Tutorial/fragShader.glsl
@@ -1,5 +1,9 @@
 #version 430
-in vec4 varyingColor;
+
+in vec3 varyingNormal;
+in vec3 varyingLightDir;
+in vec3 varyingVertPos;
+
 out vec4 fragColor;
 
 struct PositionalLight
@@ -26,5 +30,20 @@ uniform mat4 norm_matrix;
 
 void main(void)
 {
-	fragColor = varyingColor;
+	
+	vec3 L = normalize(varyingLightDir);
+	vec3 N = normalize(varyingNormal);
+	vec3 V = normalize(-varyingVertPos);
+
+	vec3 R = normalize(reflect(-L,N));
+
+	float cosTheta = dot(L,N);
+
+	float cosPhi = dot(V,R);
+
+	vec3 ambient = ((globalAmbient * material.ambient) + (light.ambient * material.ambient)).xyz;
+	vec3 diffuse = light.diffuse.xyz * material.diffuse.xyz * max(cosTheta ,0.0);
+	vec3 specular = light.specular.xyz * material.specular.xyz * pow(max(cosPhi,0.0), material.shininess);
+
+	fragColor = vec4((ambient + diffuse + specular), 1.0);
 }
--- OpenGL_Tutorial/vertShader.glsl
+++ OpenGL_Tutorial/vertShader.glsl
@@ -4,7 +4,9 @@ layout(location = 0) in vec3 vertPos;
 layout(location = 1) in vec2 texCoord;
 layout(location = 2) in vec3 vertNormal;
 
-out vec4 varyingColor;
+out vec3 varyingNormal;
+out vec3 varyingLightDir;
+out vec3 varyingVertPos;
 
 struct PositionalLight
 { 
@@ -33,28 +35,10 @@ uniform mat4 norm_matrix; // for transforming normals
 
 void main(void)
 { 
-	vec4 color;
+	
+	varyingVertPos = (mv_matrix * vec4(vertNormal,1.0)).xyz;
+	varyingLightDir = light.position - varyingVertPos;
+	varyingNormal = (norm_matrix * vec4(vertNormal,1.0)).xyz;
 
-	// convert vertex position to view space,
-	// convert normal to view space, and
-	// calculate view space light vector (from vertex to light)
-	vec4 P = mv_matrix * vec4(vertPos,1.0);
-	vec3 N = normalize((norm_matrix * vec4(vertNormal,1.0)).xyz);
-	vec3 L = normalize(light.position - P.xyz);
-
-	// view vector is equivalent to the negative of view space vertex position
-	vec3 V = normalize(-P.xyz);
-	// R is reflection of -L with respect to surface normal N
-	vec3 R = reflect(-L,N);
-
-	// ambient, diffuse, and specular contributions
-	vec3 ambient = ((globalAmbient * material.ambient) + (light.ambient * material.ambient)).xyz;
-	vec3 diffuse = light.diffuse.xyz * material.diffuse.xyz * max(dot(N,L), 0.0);
-	vec3 specular = material.specular.xyz * light.specular.xyz * pow(max(dot(R,V), 0.0f), material.shininess);
-
-	// send the color output to the fragment shader
-	varyingColor = vec4((ambient + diffuse + specular), 1.0);
-
-	// send the position to the fragment shader, as before
-	gl_Position = proj_matrix * mv_matrix * vec4(vertPos,1.0);
+	gl_Position = proj_matrix *mv_matrix * vec4(vertPos, 1.0);
 }
